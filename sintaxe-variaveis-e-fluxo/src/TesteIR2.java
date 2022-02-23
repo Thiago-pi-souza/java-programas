304e81e3ec45fb116e8c53d58108c4d0ae0ec4aa
Thiago-pi-souza <thiagopisouza@outlook.com>
2022-02-23 18:06:43 -0300

    codigo java

---
+++ sintaxe-variaveis-e-fluxo/src/TesteIR2.java
@@ -0,0 +1,24 @@
+
+public class TesteIR2 {
+
+	public static void main(String[] args) {
+		double salario = 4000.0;
+
+		if (salario >= 1900.0 && salario <= 2800.0) {
+			System.out.println("A sua IR é de 7.5%");
+			System.out.println("Pode deduzir na declaração R$ 142");
+		}
+		if (salario >= 2800.01 && salario <= 3751.0) {
+			System.out.println("A sua IR é de 15%");
+			System.out.println("Pode deduzir na declaração R$ 350");
+		}
+		if (salario >= 3751.01 && salario <= 4664.00) {
+			System.out.println("A sua IR é de 22.5%");
+			System.out.println("Pode deduzir na declaração R$ 636");
+		} 
+		else {
+			System.out.println("Não foi possivel calcular a sua IR");
+		}
+	}
+
+}
